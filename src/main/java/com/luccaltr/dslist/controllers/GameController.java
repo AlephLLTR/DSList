da276e9d5b16a1c5d0ea845a4d7fa022e532d3b2
LuccaL <106239988+AlephLLTR@users.noreply.github.com>
2025-05-21 14:52:29 -0300

    Implementado a função findById - Aula 2: 0:58:00

--- src/main/java/com/luccaltr/dslist/controllers/GameController.java
+++ src/main/java/com/luccaltr/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.luccaltr.dslist.dto.GameDTO;
 import com.luccaltr.dslist.dto.GameMinDTO;
 import com.luccaltr.dslist.services.GameService;
 
@@ -22,4 +24,12 @@ public class GameController {
         List<GameMinDTO> result = gameService.findAll();
         return result;
     }
+
+    @GetMapping(value = "/{gameId}")
+    public GameDTO findById(@PathVariable Long gameId) {
+        GameDTO result = gameService.findById(gameId);
+        return result;
+    }
+
+
 }
--- src/main/java/com/luccaltr/dslist/services/GameService.java
+++ src/main/java/com/luccaltr/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.luccaltr.dslist.dto.GameDTO;
 import com.luccaltr.dslist.dto.GameMinDTO;
 import com.luccaltr.dslist.entities.Game;
 import com.luccaltr.dslist.repositories.GameRepository;
@@ -15,10 +17,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long gameId) {
+        Game result = gameRepository.findById(gameId).get();
+        return new GameDTO(result);
+    }
+
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        List<GameMinDTO> dto_result = result.stream().map(game -> new GameMinDTO(game)).toList();
-        return dto_result;
+        return result.stream().map(game -> new GameMinDTO(game)).toList();
     }
 
 }
