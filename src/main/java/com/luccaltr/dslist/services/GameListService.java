6b9e94717c5b56a904e6953081965b01fb416f29
LuccaL <106239988+AlephLLTR@users.noreply.github.com>
2025-05-25 00:13:43 -0300

    Implementado função de rearranjo das listas, Aula 4 concluída

--- src/main/java/com/luccaltr/dslist/services/GameListService.java
+++ src/main/java/com/luccaltr/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.luccaltr.dslist.dto.GameListDTO;
 import com.luccaltr.dslist.entities.GameList;
+import com.luccaltr.dslist.projections.GameMinProjection;
 import com.luccaltr.dslist.repositories.GameListRepository;
+import com.luccaltr.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,10 +18,30 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(object -> new GameListDTO(object)).toList();
+    }
+
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 
 }
